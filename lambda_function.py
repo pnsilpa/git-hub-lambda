d2eb5cc4996ee756d6d4be845de7402fab44674c
pnsilpa <156974893+pnsilpa@users.noreply.github.com>
2024-04-16 09:07:14 -0500

    Add files via upload
    Adding CICD files for AWS Lambda

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+import requests
+
+def lambda_handler(event, context):
+    url = "https://api.quotable.io/random"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        quote_data = response.json()
+        content = quote_data.get("content")
+        author = quote_data.get("author")
+        print(f"Random Quote: '{content}' - {author}")
+        print("v1.1")
+    else:
+        print(f"Failed to retrieve a random quote. Status code: {response.status_code}")
+
